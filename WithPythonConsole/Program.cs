3c53ab7bb92b4f0bebc6be3006dbc65965ef5b12
장문성/선임연구원/Protocol실 1팀(ms.jang@lge.com) <ms.jang@lge.com>
2019-06-27 13:25:01 +0900

    "add dynamic"

---
+++ WithPythonConsole/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
+using IronPython.Hosting;
+
+namespace WithPythonConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create Script engine.
+            ScriptEngine engine = Python.CreateEngine();
+            //Create Script scope
+            ScriptScope scope = engine.CreateScope();
+
+            //Set variable in scope
+            scope.SetVariable("n", "박상현");
+            scope.SetVariable("p", "010-123-4566");
+
+            //Create scriptsouce
+            ScriptSource source = engine.CreateScriptSourceFromString(
+                @"
+class NameCard :
+    name = ''
+    phone = ''
+
+    def __init__(self, name, phone) : 
+        self.name = name 
+        self.phone = phone
+
+    def printNameCard(self) : 
+        print self.name + ', ' + self.phone
+
+NameCard(n, p)
+");
+            dynamic result = source.Execute(scope);
+            result.printNameCard();
+
+            Console.WriteLine("{0}, {1}", result.name, result.phone);
+        }
+    }
+}
