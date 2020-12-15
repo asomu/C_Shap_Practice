5086982ce40d888324ff611cc2b5f00957f62553
Moonsung Jang <asomu34@hotmail.com>
2020-12-15 22:01:20 +0900

    20.12.15

---
+++ FileAndDirectory/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using static System.Console;
+namespace FileAndDirectory
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (BinaryWriter bw = new BinaryWriter(new FileStream("a.dat", FileMode.Create)))
+            {
+                bw.Write(int.MaxValue);
+                bw.Write("Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?Good Morning 야후 이걸 250자가 넘으면 어떻게 될까?");
+                bw.Write(float.MaxValue);
+            }
+            using (BinaryReader br = new BinaryReader(new FileStream("a.dat", FileMode.Open)))
+            {
+                WriteLine($"File size : {br.BaseStream.Length} Byte");
+                WriteLine($"{br.ReadInt32()}");
+                WriteLine($"{br.ReadString()}");
+            }
+        }
+
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {        
+            
+                       
+            int[] numbers = { 9, 2, 60, 4, 3};
+
+            var result = (from n in numbers
+                          where n > 8
+                          orderby n
+                          select n).First()
+                          ;
+
+
+                Console.WriteLine(result);
+
+            string myString = new string("first");
+            string[] yourString = new string[10];
+            yourString[0] = "first";
+            string[] myString2 = new string[3] { "first", "second", "third" };
+            var result2 = myString2.Single((n)=>n.Length>3);
+            //foreach (var item in result2)
+            //{
+            //    Console.WriteLine(item);
+
+            //}
+            Console.WriteLine(result2);
+        }
+    }
+}
